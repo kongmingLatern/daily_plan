199ace33bd6ca21a3efa90b170a5843eb62716ef
凤之兮原 <1577310596@qq.com>
2023-01-11 14:07:12 +0800

    chore: clear

--- index.js
+++ index.js
@@ -1,28 +1,23 @@
-const { Octokit } = require("octokit");
-const core = require("@actions/core");
-const dayjs = require("dayjs");
-// nodejs12
+const { Octokit } = require('octokit')
+const core = require('@actions/core')
+const dayjs = require('dayjs')
 
-const token = core.getInput("token");
+const token = core.getInput('token')
 const octokit = new Octokit({
   auth: token,
-});
-
-// octokit api create issue
+})
 
 octokit.rest.issues.create({
-  owner: "cuixiaorui",
-  repo: "teach-create-issues-action",
+  owner: 'cuixiaorui',
+  repo: 'teach-create-issues-action',
   title: getTitle(),
   body: getBody(),
-});
-
-// YYYY-MM-DD
+})
 
 function getTitle() {
-  return dayjs().format("YYYY-MM-DD");
+  return dayjs().format('YYYY-MM-DD')
 }
 
 function getBody() {
-  return "[如何写每日任务](https://github.com/cuixiaorui/study-every-day/blob/main/sed/daily-task.md)";
+  return '[如何写每日任务](https://github.com/cuixiaorui/study-every-day/blob/main/sed/daily-task.md)'
 }
